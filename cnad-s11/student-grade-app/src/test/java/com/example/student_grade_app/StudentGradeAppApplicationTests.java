f5f686908f530a113ce027fe67a2772c397afc50
Yellanki-Hitesh <2420030567@klh.edu.in>
2026-09-18 13:48:00 +0530

    Add files via upload

---
+++ cnad-s11/student-grade-app/src/test/java/com/example/student_grade_app/StudentGradeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_grade_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
