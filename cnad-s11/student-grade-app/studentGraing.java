1c0db935efd2ac4b82bc6a5d6c20c63ea3369b11
Yellanki-Hitesh <2420030567@klh.edu.in>
2026-09-18 13:51:17 +0530

    Add files via upload

---
+++ cnad-s11/student-grade-app/studentGraing.java
@@ -0,0 +1,36 @@
+public class studentGraing {
+    public void testTotal() {
+
+        Student student =
+                new Student("Rahul", 85, 78, 82);
+    
+        GradeCalculator calculator =
+                new GradeCalculator();
+    
+        assertEquals(245, calculator.calculateTotal(student));
+    }
+    
+    @Test
+    public void testGrade() {
+    
+        Student student =
+                new Student("Rahul", 85, 78, 82);
+    
+        GradeCalculator calculator =
+                new GradeCalculator();
+    
+        assertEquals("A", calculator.calculateGrade(student));
+    }
+    
+    @Test
+    public void testPass() {
+    
+        Student student =
+                new Student("Rahul", 85, 78, 82);
+    
+        GradeCalculator calculator =
+                new GradeCalculator();
+    
+        assertEquals("PASS", calculator.calculateResult(student));
+    }
+}
